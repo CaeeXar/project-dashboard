2935327e4a74f45ae56df5a2ff780c87d8ca8416
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-04-25 14:13:16 +0200

    permission test

--- lib/test.ts
+++ lib/test.ts
@@ -1,14 +1,15 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
-
-
+import * as fs from 'fs';
 
 export default async function loadData() {
-    return open({
-        filename: process.env.DB_PATH || "",
-        driver: sqlite3.Database
-    }).then(async (db) => {
-        return await db.get('SELECT * FROM test');
-    });
+    return fs.readFileSync(process.env.DB_PATH || "", "utf-8");
+
+    // return open({
+    //     filename: process.env.DB_PATH || "",
+    //     driver: sqlite3.Database
+    // }).then(async (db) => {
+    //     return await db.get('SELECT * FROM test');
+    // });
 
 };
