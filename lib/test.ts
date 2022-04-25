73e450e53823d587d84946e20104c674b5569c43
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-04-25 13:49:10 +0200

    db get test

--- lib/test.ts
+++ lib/test.ts
@@ -1,3 +1,14 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+
+
 export default async function loadData() {
-    return process.env.DB_PATH;
-}
+    return open({
+        filename: process.env.DB_PATH || "",
+        driver: sqlite3.Database
+    }).then(async (db) => {
+        return await db.get('SELECT * FROM test');
+    });
+
+};
